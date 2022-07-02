f7fe77b192b0d20a5e3cf00b6bbf18b434bbf2b8
hadadadebadada <artur_pfeifer2@gmx.de>
2022-07-02 16:28:54 +0200

    init proj

---
+++ client/src/App.jsx
@@ -0,0 +1,139 @@
+/* import { EthProvider } from "./contexts/EthContext";
+import Intro from "./components/Intro/";
+import Setup from "./components/Setup";
+import Demo from "./components/Demo";
+import Footer from "./components/Footer"; */
+import "./App.css";
+import React, { Component, useState } from "react";
+import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import getWeb3 from "./getWeb3";
+
+import Pixelart from "./components/Pixelart";
+import "./App.css";
+
+import Animations from "./components/Animations"
+
+class App extends Component{
+  
+  constructor(props) {
+    super(props)
+   // this.ref = React.createRef()
+  }
+  state = { storageValue: 0, web3: null, accounts: null, contract: null, value: "adsd", image: "" };
+/* 
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+
+
+  } */
+
+  runExample = async () => {
+    const { accounts, contract } = this.state;
+      const response = await contract.methods.read().call();
+      console.log(response)
+  };
+
+  render(){
+    return <>
+    <div>
+
+    
+    <Pixelart></Pixelart>
+
+    {/* <Animations></Animations> */}
+    
+    </div>
+
+
+    </>
+  }
+
+
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function App() {
+  return (
+    <EthProvider>
+      <div id="App" >
+        <div className="container">
+          <Intro />
+          <hr />
+          <Setup />
+          <hr />
+          <Demo />
+          <hr />
+          <Footer />
+        </div>
+      </div>
+    </EthProvider>
+  );
+}
+
+export default App; */
---
+++ client/src/components/Pixelart.js
@@ -0,0 +1,134 @@
+import React, { Component, useState } from 'react'
+import "./Pixelart.css"
+import html2canvas from "html2canvas"
+//import { color } from 'html2canvas/dist/types/css/types/color';
+//import { color } from 'html2canvas/dist/types/css/types/color';
+
+class Pixelart extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colorPicker: "#ffffff"
+    };
+   
+  }
+  componentDidMount() {
+
+
+
+    let draw = false
+    let container = document.querySelector('.container')
+    let sizeEl = document.querySelector(".size");
+    let size = sizeEl.value
+    const color = document.querySelector('.color')
+    let resetBtn = document.querySelector('.btn')
+    let eraserBtn = document.querySelector('.eraserBtn')
+
+
+    this.setState({ colorPicker: color.value }, () => {
+
+      function populate(size) {
+
+        container.style.setProperty('--size', size)
+
+        for (let i = 0; i < size * size; i++) {
+          let div = document.createElement('div')
+          div.classList.add('pixel')
+
+          let colorPicker = getComputedStyle(div)
+            .getPropertyValue('--main-bg-color');
+
+          div.addEventListener('mouseover', function () {
+            div.style.setProperty("--main-bg-color", color.value);
+
+
+            if (!draw) return
+            div.style.backgroundColor = color.value
+            div.style.setProperty("--main-bg-color", color.value);
+            div.style.setProperty("--pixel-bg-color", color.value);
+
+
+          })
+
+          div.addEventListener('mouseover', function () {
+
+            if (getComputedStyle(div)
+              .getPropertyValue('--pixel-bg-color') != getComputedStyle(div)
+                .getPropertyValue('--main-bg-color')) return
+            div.style.backgroundColor = colorPicker;
+          })
+
+          div.addEventListener('mousedown', function () {
+            div.style.backgroundColor = color.value
+            div.style.setProperty("--main-bg-color", color.value);
+
+          })
+
+          container.appendChild(div)
+        }
+      }
+
+
+
+      window.addEventListener("mousedown", function () {
+        draw = true
+      })
+      window.addEventListener("mouseup", function () {
+        draw = false
+      })
+
+      function reset() {
+        container.innerHTML = ''
+        populate(size)
+      }
+
+      function setColorWhite() {
+        color.value = "#ffffff"
+      }
+
+      resetBtn.addEventListener('click', reset)
+      eraserBtn.addEventListener('click', setColorWhite)
+
+      populate(size);
+
+    });
+  }
+
+  appendScreenshot = () => {
+
+    const container = document.querySelector("#container");
+
+    html2canvas(container).then((canvas) => {
+      const url = canvas.toDataURL(); //png file
+
+      console.log(url) //  base64 
+
+      /* document.querySelector("#container2").appendChild(canvas); */
+    });
+  }
+
+  render() {
+    return <div className="Appi">
+
+      <div className="navbar">
+        <button className="btn">Reset</button>
+        <button className="eraserBtn">Eraser</button>
+        <input type="color" valueDefault="#00eeff" id="color" class="color"></input>
+        <input type="number" value="64" class="size"></input>
+     </div>
+
+      <div className="container" id="container"></div>
+
+      <button onClick={this.appendScreenshot}>Load Canvas</button>
+
+     {/*   <div className="container2" id="container2"></div> */}
+
+
+    </div>;
+
+  }
+}
+
+export default Pixelart;
